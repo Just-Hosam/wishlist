42ea7038f44bcd49b89c901180a69f317cb33480
Just-Hosam <hosam_firas@hotmail.com>
2025-09-06 14:07:10 -0600

    Full width popover buttons

--- components/layout/NavigationPopover.tsx
+++ components/layout/NavigationPopover.tsx
@@ -28,27 +28,27 @@ export default function NavigationPopover() {
           </PopoverTrigger>
         </AuthCheck>
         <PopoverContent className="w-fit mr-6">
-          <div className="flex flex-col items-start">
+          <div className="flex flex-col">
             <Link href="/wishlist" onClick={() => setPopoverOpen(false)}>
-              <Button className="w-full" variant="ghost">
+              <Button className="w-full justify-start" variant="ghost">
                 <ScrollText />
                 Wishlist
               </Button>
             </Link>
             <Link href="/owned" onClick={() => setPopoverOpen(false)}>
-              <Button className="w-full" variant="ghost">
+              <Button className="w-full justify-start" variant="ghost">
                 <FolderCheck />
                 Owned
               </Button>
             </Link>
             <Link href="/completed" onClick={() => setPopoverOpen(false)}>
-              <Button className="w-full" variant="ghost">
+              <Button className="w-full justify-start" variant="ghost">
                 <CircleCheckBig />
                 Completed
               </Button>
             </Link>
             <Link href="/graveyard" onClick={() => setPopoverOpen(false)}>
-              <Button className="w-full" variant="ghost">
+              <Button className="w-full justify-start" variant="ghost">
                 <Skull />
                 Graveyard
               </Button>
