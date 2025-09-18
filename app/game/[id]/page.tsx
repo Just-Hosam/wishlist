aebd24473c0a98a337c138110707bc425f24757b
Just-Hosam <hosam_firas@hotmail.com>
2025-09-17 19:41:19 -0600

    Remove back button from game page

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -19,38 +19,35 @@ export default async function Game({ params }: Props) {
   const game = await prisma?.game.findUnique({ where: { id } })
 
   return (
-    <div>
-      <div className="sticky top-[88px] flex items-center justify-between gap-4 bg-white pb-4">
-        <BackButton />
-        <div className="flex items-center gap-2">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button size="icon" variant="ghost">
-                <EllipsisVertical />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="mr-6 w-fit md:mr-0">
-              <div className="flex flex-col">
-                <Link href={`/game/${id}/edit`}>
-                  <Button className="w-full justify-start" variant="ghost">
-                    <Pencil />
-                    Edit
-                  </Button>
-                </Link>
-                <DeleteGameButton gameId={id} />
-              </div>
-            </PopoverContent>
-          </Popover>
+    <>
+      <header className="flex justify-between gap-4">
+        <div className="flex-1">
+          <h2 className="text-3xl font-semibold">{game?.name}</h2>
+          {game?.length && (
+            <div className="mt-2 flex items-center gap-1 text-sm text-gray-600">
+              <Clock size={14} /> {game?.length} hours
+            </div>
+          )}
         </div>
-      </div>
-      <header>
-        <h2 className="text-3xl font-semibold">{game?.name}</h2>
-        {game?.length && (
-          <div className="mt-2 flex items-center gap-1 text-sm text-gray-600">
-            <Clock size={14} /> {game?.length} hours
-          </div>
-        )}
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="icon" variant="ghost">
+              <EllipsisVertical />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="mr-6 w-fit md:mr-0">
+            <div className="flex flex-col">
+              <Link href={`/game/${id}/edit`}>
+                <Button className="w-full justify-start" variant="ghost">
+                  <Pencil />
+                  Edit
+                </Button>
+              </Link>
+              <DeleteGameButton gameId={id} />
+            </div>
+          </PopoverContent>
+        </Popover>
       </header>
-    </div>
+    </>
   )
 }
