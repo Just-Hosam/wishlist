035332e158afaaf240a4b8932f414984b7dde52b
Just-Hosam <hosam_firas@hotmail.com>
2025-10-28 12:44:00 -0600

    Memoize the lists

--- components/lists/ListsWrapper.tsx
+++ components/lists/ListsWrapper.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useMemo } from "react"
+
 import WishlistList from "@/components/lists/WishlistList"
 import { useTabContext } from "@/contexts/TabContext"
 import { GameCategory } from "@/types/game"
@@ -14,20 +16,21 @@ interface ListsWrapperProps {
 export default function ListsWrapper({ data }: ListsWrapperProps) {
   const { activeTab } = useTabContext()
 
-  return (
-    <>
-      {activeTab === GameCategory.WISHLIST && (
-        <WishlistList games={data.wishlistGames} />
-      )}
-      {activeTab === GameCategory.LIBRARY && (
-        <LibraryList games={data.libraryGames} />
-      )}
-      {activeTab === GameCategory.COMPLETED && (
-        <CompletedList games={data.completedGames} />
-      )}
-      {activeTab === GameCategory.ARCHIVED && (
-        <ArchivedList games={data.archivedGames} />
-      )}
-    </>
+  const memoizedLists = useMemo(
+    () =>
+      ({
+        [GameCategory.WISHLIST]: <WishlistList games={data.wishlistGames} />,
+        [GameCategory.LIBRARY]: <LibraryList games={data.libraryGames} />,
+        [GameCategory.COMPLETED]: <CompletedList games={data.completedGames} />,
+        [GameCategory.ARCHIVED]: <ArchivedList games={data.archivedGames} />
+      }) as Record<GameCategory, JSX.Element>,
+    [
+      data.wishlistGames,
+      data.libraryGames,
+      data.completedGames,
+      data.archivedGames
+    ]
   )
+
+  return memoizedLists[activeTab] ?? null
 }
