fab22f0c13e126b5ac5b8f07ad6a998c77eb2951
Just-Hosam <hosam_firas@hotmail.com>
2025-12-30 00:01:16 -0700

    Remove old imports

--- app/game/[id]/add/layout.tsx
+++ app/game/[id]/add/layout.tsx
@@ -1,6 +1,4 @@
 import { PageHeader } from "@/components/layout/PageHeader"
-import { Button } from "@/components/ui/button"
-import { Save } from "lucide-react"
 
 export default async function AddLayout({
   children
