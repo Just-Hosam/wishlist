110219e4e4ee608f079ad7d3e461c2e91994f57b
Just-Hosam <hosam_firas@hotmail.com>
2025-12-31 13:36:13 -0700

    Add loading skeleton for time to beat in Game component

--- components/layout/Game.tsx
+++ components/layout/Game.tsx
@@ -7,6 +7,9 @@ import {
   buildPlayStationStoreUrl
 } from "@/lib/igdb-store-links"
 import NintendoLinkInput from "./NintendoLinkInput"
+import { Suspense } from "react"
+import { fetchTimeToBeat } from "@/server/actions/igdb"
+import { Skeleton } from "../ui/skeleton"
 
 interface Props {
   imageId: string
@@ -15,6 +18,7 @@ interface Props {
   summary?: string
   igdbPlaystationUrlSegment?: string
   igdbNintendoUrlSegment?: string
+  igdbId?: string | null
 }
 
 export function Game({
@@ -23,23 +27,26 @@ export function Game({
   length,
   summary,
   igdbPlaystationUrlSegment,
-  igdbNintendoUrlSegment
+  igdbNintendoUrlSegment,
+  igdbId
 }: Props) {
   return (
     <div>
       <header className="mx-auto flex flex-col items-center text-center">
-        <div className="mb-5 h-[240px] w-[180px] overflow-hidden rounded-2xl bg-gray-200 shadow-lg">
+        <div className="mb-5 h-[213px] w-[160px] overflow-hidden rounded-2xl bg-gray-200 shadow-xl">
           <Image
             src={buildIGDBImageUrl(imageId || "", "1080p")}
             alt={name || "Game cover"}
-            width={180}
-            height={240}
+            width={160}
+            height={213}
           />
         </div>
         <h1 className="text-2xl font-medium">{name}</h1>
         <p className="mt-1 flex items-center gap-1 text-sm text-muted-foreground">
           <Clock size={12} strokeWidth={1.75} className="mt-[-0.5px]" />
-          {length ? `${length} hours` : "-"}
+          <Suspense fallback={<Skeleton className="h-3 w-16" />}>
+            <TimeToBeat length={length || null} igdbId={igdbId || null} />
+          </Suspense>
         </p>
       </header>
 
@@ -72,4 +79,21 @@ export function Game({
       </div>
     </div>
   )
+}
+
+async function TimeToBeat({
+  length,
+  igdbId
+}: {
+  length: number | null
+  igdbId: string | null
+}) {
+  if (length) return <span>{length} hours</span>
+
+  if (!igdbId) return <span>-</span>
+
+  const timeToBeat = await fetchTimeToBeat(igdbId)
+  if (!timeToBeat) return <span>-</span>
+
+  return <span>{timeToBeat} hours</span>
 }
