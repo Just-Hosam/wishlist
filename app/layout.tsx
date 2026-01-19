b9ac031d51ff3bc4169537a9d1c6c70160c1d254
Just-Hosam <hosam_firas@hotmail.com>
2026-01-19 13:18:38 -0700

    Revert container in body

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,12 +39,14 @@ export default async function RootLayout({
               src="//unpkg.com/react-scan/dist/auto.global.js"
             ></script> */}
       </head>
-      <body
-        className="m-auto mt-[76px] max-w-[700px] overflow-y-auto px-6 pb-32 pt-3"
-        style={{ height: "calc(100% - 76px)" }}
-        data-scroll-container
-      >
-        {children}
+      <body>
+        <div
+          className="m-auto mt-[76px] max-w-[700px] overflow-y-auto px-6 pb-32 pt-3"
+          style={{ height: "calc(100% - 76px)" }}
+          data-scroll-container
+        >
+          {children}
+        </div>
         <FooterWrapper />
         <Toaster position="top-center" />
       </body>
