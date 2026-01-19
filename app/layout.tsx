9ae50f18cb8a66489cef7601b51ebb5ccd3c888b
Just-Hosam <hosam_firas@hotmail.com>
2026-01-19 13:14:12 -0700

    Revert scroll behaviour

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export default async function RootLayout({
       <body
         className="m-auto mt-[76px] max-w-[700px] overflow-y-auto px-6 pb-32 pt-3"
         style={{ height: "calc(100% - 76px)" }}
+        data-scroll-container
       >
         {children}
         <FooterWrapper />
