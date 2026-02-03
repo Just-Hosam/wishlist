cfb3bf551511d06a3e2bde94eddc9d7258d6c529
Just-Hosam <hosam_firas@hotmail.com>
2026-02-03 12:07:55 -0700

    Update more page

--- app/more/page.tsx
+++ app/more/page.tsx
@@ -1,32 +1,68 @@
 import { SignOutButton } from "@/components/layout/SignoutButton"
-import { Button } from "@/components/ui/button"
-import { CheckCircle2, Info } from "lucide-react"
 import { Link } from "@/components/navigation"
+import { Button } from "@/components/ui/button"
+import { ArrowRight, CheckCircle2, Info } from "lucide-react"
 
 export default function MorePage() {
   return (
     <div className="custom-slide-fade-in">
-      <Link
-        href="/more/about"
-        style={{ animationDelay: "0ms", animationFillMode: "backwards" }}
+      <h3 className="mb-2 text-sm font-semibold text-muted-foreground">
+        Pages
+      </h3>
+      <Button
+        asChild
+        className="w-full justify-start"
+        size="lg"
+        variant="ghost"
       >
-        <Button className="w-full justify-start" variant="ghost">
+        <Link href="/more/about">
           <Info />
           About
-        </Button>
-      </Link>
-      <Link
-        href="/more/completed"
-        style={{ animationDelay: "50ms", animationFillMode: "backwards" }}
+          <ArrowRight className="ml-auto text-muted-foreground" />
+        </Link>
+      </Button>
+      <Button
+        asChild
+        className="w-full justify-start"
+        size="lg"
+        variant="ghost"
       >
-        <Button className="w-full justify-start" variant="ghost">
+        <Link href="/more/completed">
           <CheckCircle2 />
           Completed
-        </Button>
-      </Link>
-      <div style={{ animationDelay: "100ms", animationFillMode: "backwards" }}>
-        <SignOutButton />
-      </div>
+          <ArrowRight className="ml-auto text-muted-foreground" />
+        </Link>
+      </Button>
+      <div className="-mx-6 my-6 border-[3px]"></div>
+      <h3 className="mb-2 text-sm font-semibold text-muted-foreground">
+        Settings
+      </h3>
+      <Button
+        asChild
+        className="w-full justify-start"
+        size="lg"
+        variant="ghost"
+      >
+        <Link href="/more/about">
+          <Info />
+          About
+          <ArrowRight className="ml-auto text-muted-foreground" />
+        </Link>
+      </Button>
+      <Button
+        asChild
+        className="w-full justify-start"
+        size="lg"
+        variant="ghost"
+      >
+        <Link href="/more/completed">
+          <CheckCircle2 />
+          Completed
+          <ArrowRight className="ml-auto text-muted-foreground" />
+        </Link>
+      </Button>
+      <div className="-mx-6 my-6 border-[3px]"></div>
+      <SignOutButton />
     </div>
   )
 }
--- components/layout/SignoutButton.tsx
+++ components/layout/SignoutButton.tsx
@@ -1,15 +1,21 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 import { LogOutIcon } from "lucide-react"
 import { signOut } from "next-auth/react"
 
-export function SignOutButton() {
+type SignOutButtonProps = {
+  className?: string
+}
+
+export function SignOutButton({ className }: SignOutButtonProps) {
   return (
     <Button
       onClick={() => signOut()}
-      variant="ghost"
-      className="w-full justify-start text-destructive"
+      variant="destructive"
+      size="lg"
+      className={cn("w-full justify-start", className)}
     >
       <LogOutIcon />
       Logout
