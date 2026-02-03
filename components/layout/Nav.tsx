9dbfcb5cced8e3bd803bcfb2b3080c6c1613aee5
Just-Hosam <hosam_firas@hotmail.com>
2026-02-03 14:35:32 -0700

    I couldn't fix it

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -8,7 +8,7 @@ export function Nav({ children }: NavProps) {
   return (
     <nav
       className={cn(
-        "absolute left-0 right-0 top-0 z-30 m-auto flex min-h-[84px] max-w-[1200px] items-center gap-3 bg-gradient-to-b from-white via-white/65 via-55% to-transparent px-5 pb-6 pt-3"
+        "absolute left-0 right-0 top-0 z-30 m-auto flex min-h-[84px] max-w-[1200px] items-center gap-3 bg-gradient-to-b from-[#fafafa] via-[#fafafa]/65 via-55% to-transparent px-5 pb-6 pt-3"
       )}
     >
       {!!children && children}
