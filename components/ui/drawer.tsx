18ccadc069ed61a19607107fba3a018107728251
Just-Hosam <hosam_firas@hotmail.com>
2026-02-06 12:14:46 -0700

    More space in drawer actions

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -68,7 +68,7 @@ const DrawerFooter = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn("mt-auto flex flex-col gap-2 p-4", className)}
+    className={cn("mt-auto flex flex-col gap-2 p-4 pb-5", className)}
     {...props}
   />
 )
