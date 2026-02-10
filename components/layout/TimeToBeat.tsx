be0a86361a48f44df58d4d6b0010ce9a6bdf332e
Just-Hosam <hosam_firas@hotmail.com>
2026-02-10 13:47:31 -0700

    Add title to time to beat comp

--- components/layout/TimeToBeat.tsx
+++ components/layout/TimeToBeat.tsx
@@ -5,39 +5,49 @@ interface Props {
   story?: number
   extra?: number
   complete?: number
+  title?: string
   loading?: boolean
 }
 
-export default function TimeToBeat({ story, extra, complete, loading }: Props) {
+export default function TimeToBeat({
+  story,
+  extra,
+  complete,
+  title,
+  loading
+}: Props) {
   return (
-    <div className="flex items-center gap-4 rounded-3xl border px-4 py-3">
-      <div className="flex-1">
-        <label className="text-xs text-muted-foreground">Story</label>
-        {loading ? (
-          <Skeleton className="mt-1 h-5 w-16" />
-        ) : (
-          <p className="mt-1 text-sm">{story ? <>{story} hours</> : "-"}</p>
-        )}
-      </div>
-      <div className="w-px self-stretch bg-border" />
-      <div className="flex-1">
-        <label className="text-xs text-muted-foreground">Extra</label>
-        {loading ? (
-          <Skeleton className="mt-1 h-5 w-16" />
-        ) : (
-          <p className="mt-1 text-sm">{extra ? <>{extra} hours</> : "-"}</p>
-        )}
-      </div>
-      <div className="w-px self-stretch bg-border" />
-      <div className="flex-1">
-        <label className="text-xs text-muted-foreground">Complete</label>
-        {loading ? (
-          <Skeleton className="mt-1 h-5 w-16" />
-        ) : (
-          <p className="mt-1 text-sm">
-            {complete ? <>{complete} hours</> : "-"}
-          </p>
-        )}
+    <div className="rounded-3xl border px-5 py-4">
+      {title && <h4 className="mb-1 text-xs font-bold">{title}</h4>}
+      <div className="flex items-center gap-4">
+        <div className="flex-1">
+          <label className="text-xs text-muted-foreground">Story</label>
+          {loading ? (
+            <Skeleton className="mt-1 h-5 w-16" />
+          ) : (
+            <p className="mt-1 text-sm">{story ? <>{story} hours</> : "-"}</p>
+          )}
+        </div>
+        <div className="w-px self-stretch bg-border" />
+        <div className="flex-1">
+          <label className="text-xs text-muted-foreground">Extra</label>
+          {loading ? (
+            <Skeleton className="mt-1 h-5 w-16" />
+          ) : (
+            <p className="mt-1 text-sm">{extra ? <>{extra} hours</> : "-"}</p>
+          )}
+        </div>
+        <div className="w-px self-stretch bg-border" />
+        <div className="flex-1">
+          <label className="text-xs text-muted-foreground">Complete</label>
+          {loading ? (
+            <Skeleton className="mt-1 h-5 w-16" />
+          ) : (
+            <p className="mt-1 text-sm">
+              {complete ? <>{complete} hours</> : "-"}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   )
