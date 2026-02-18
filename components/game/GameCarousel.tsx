f23e85c005b03a352259dd4490c1a4670bb582ff
Just-Hosam <hosam_firas@hotmail.com>
2026-02-18 15:34:12 -0700

    Slightly cleaner structure for scroll

--- components/game/GameCarousel.tsx
+++ components/game/GameCarousel.tsx
@@ -30,93 +30,86 @@ export default function GameCarousel({
 }: Props) {
   return (
     <div
-      className="hide-scrollbar -mx-4 snap-x snap-mandatory overflow-x-auto scroll-smooth"
+      className="hide-scrollbar -mx-4 flex snap-x snap-mandatory overflow-x-auto scroll-smooth px-4"
       tabIndex={0}
       role="region"
-      aria-label="Now Playing Games"
+      aria-label="Game Carousel"
     >
-      <div className="flex">
-        {games.map((game, index) => (
-          <div
-            className="w-[166px] shrink-0 snap-start pl-4 last:w-[182px] last:pr-4"
-            key={game.id}
-          >
-            <Link href={`${baseHref}/${game.id}`}>
-              <div className="relative mb-2 w-full overflow-hidden rounded-xl bg-gray-200 shadow-md">
-                {game.coverImageUrl && (
-                  <Image
-                    src={game.coverImageUrl}
-                    alt={`${game.name} cover`}
-                    className="object-cover"
-                    priority={index < 4}
-                    width={150}
-                    height={200}
-                  />
-                )}
-              </div>
+      {games.map((game, index) => (
+        <div className="w-[166px] shrink-0 snap-start pl-4" key={game.id}>
+          <Link href={`${baseHref}/${game.id}`}>
+            <div className="relative mb-2 w-full overflow-hidden rounded-xl bg-gray-200 shadow-md">
+              {game.coverImageUrl && (
+                <Image
+                  src={game.coverImageUrl}
+                  alt={`${game.name} cover`}
+                  className="object-cover"
+                  priority={index < 4}
+                  width={150}
+                  height={200}
+                />
+              )}
+            </div>
 
-              <div className="px-1">
-                <h3 className="line-clamp-2 text-sm font-medium leading-tight">
-                  {game.name}
-                </h3>
+            <div className="px-1">
+              <h3 className="line-clamp-2 text-sm font-medium leading-tight">
+                {game.name}
+              </h3>
 
-                {showDate && game.releaseDate && (
-                  <p className="mt-1 text-xs text-muted-foreground md:text-sm">
-                    {formatReleaseDate(game.releaseDate)}
-                  </p>
-                )}
+              {showDate && game.releaseDate && (
+                <p className="mt-1 text-xs text-muted-foreground md:text-sm">
+                  {formatReleaseDate(game.releaseDate)}
+                </p>
+              )}
 
-                {showLength && (
-                  <p className="mt-1 flex items-center gap-1 text-xs text-muted-foreground md:text-sm">
-                    <Clock size={11} />
-                    {game.length ? `${game.length} hours` : "-"}
-                  </p>
-                )}
+              {showLength && (
+                <p className="mt-1 flex items-center gap-1 text-xs text-muted-foreground md:text-sm">
+                  <Clock size={11} />
+                  {game.length ? `${game.length} hours` : "-"}
+                </p>
+              )}
 
-                {/* Platforms - Show unique platform icons */}
-                {showPlatforms &&
-                  game.platforms &&
-                  game.platforms.length > 0 && (
-                    <div className="mt-3 flex items-center gap-2">
-                      {game.platforms.includes(Platform.PC) && (
-                        <Image
-                          src="/logos/steam.svg"
-                          alt="Steam Logo"
-                          width={13}
-                          height={13}
-                        />
-                      )}
-                      {game.platforms.includes(Platform.PLAYSTATION) && (
-                        <Image
-                          src="/logos/playstation.svg"
-                          alt="PlayStation Logo"
-                          width={13}
-                          height={13}
-                        />
-                      )}
-                      {game.platforms.includes(Platform.NINTENDO) && (
-                        <Image
-                          src="/logos/nintendo-switch.svg"
-                          alt="Nintendo Switch Logo"
-                          width={13}
-                          height={13}
-                        />
-                      )}
-                      {game.platforms.includes(Platform.XBOX) && (
-                        <Image
-                          src="/logos/xbox.svg"
-                          alt="Xbox"
-                          width={13}
-                          height={13}
-                        />
-                      )}
-                    </div>
+              {/* Platforms - Show unique platform icons */}
+              {showPlatforms && game.platforms && game.platforms.length > 0 && (
+                <div className="mt-3 flex items-center gap-2">
+                  {game.platforms.includes(Platform.PC) && (
+                    <Image
+                      src="/logos/steam.svg"
+                      alt="Steam Logo"
+                      width={13}
+                      height={13}
+                    />
                   )}
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
+                  {game.platforms.includes(Platform.PLAYSTATION) && (
+                    <Image
+                      src="/logos/playstation.svg"
+                      alt="PlayStation Logo"
+                      width={13}
+                      height={13}
+                    />
+                  )}
+                  {game.platforms.includes(Platform.NINTENDO) && (
+                    <Image
+                      src="/logos/nintendo-switch.svg"
+                      alt="Nintendo Switch Logo"
+                      width={13}
+                      height={13}
+                    />
+                  )}
+                  {game.platforms.includes(Platform.XBOX) && (
+                    <Image
+                      src="/logos/xbox.svg"
+                      alt="Xbox"
+                      width={13}
+                      height={13}
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
