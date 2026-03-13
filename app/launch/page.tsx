68f6f74baae80d29e0eabc47d920062d9141f27a
Just-Hosam <hosam_firas@hotmail.com>
2026-03-13 16:28:00 -0600

    Just some visual testing

--- app/launch/page.tsx
+++ app/launch/page.tsx
@@ -5,7 +5,7 @@ export default function LaunchPage() {
   return (
     <div className="custom-fade-in flex min-h-[400px] items-center justify-center">
       <Redirect />
-      <Spinner className="my-0" />
+      <Spinner className="my-0 bg-red-600" />
     </div>
   )
 }
