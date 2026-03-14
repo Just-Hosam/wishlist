a9e66e3621128bcfcc78a9f313a7f8f902a63c9a
Just-Hosam <hosam_firas@hotmail.com>
2026-03-14 09:33:59 -0600

    Simplify /launch a bit

--- app/launch/Redirect.tsx
+++ app/launch/Redirect.tsx
@@ -7,19 +7,7 @@ export default function Redirect() {
   const router = useRouter()
 
   useEffect(() => {
-    router.prefetch("/wishlist")
-
-    let frame2 = 0
-    const frame1 = requestAnimationFrame(() => {
-      frame2 = requestAnimationFrame(() => {
-        router.replace("/wishlist")
-      })
-    })
-
-    return () => {
-      cancelAnimationFrame(frame1)
-      cancelAnimationFrame(frame2)
-    }
+    router.replace("/wishlist")
   }, [router])
 
   return null
--- app/launch/page.tsx
+++ app/launch/page.tsx
@@ -1,11 +1,5 @@
-import Spinner from "@/components/ui/spinner"
 import Redirect from "./Redirect"
 
 export default function LaunchPage() {
-  return (
-    <div className="custom-fade-in flex min-h-[400px] items-center justify-center">
-      <Redirect />
-      <Spinner className="my-0 bg-red-600" />
-    </div>
-  )
+  return <Redirect />
 }
