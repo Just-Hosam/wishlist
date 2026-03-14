f5d81f78b17d2548d9f8d0a3d477e9c18f46493f
Just-Hosam <hosam_firas@hotmail.com>
2026-03-14 10:52:48 -0600

    Revert /launch simplification

--- app/launch/page.tsx
+++ app/launch/page.tsx
@@ -1,5 +1,11 @@
+import Spinner from "@/components/ui/spinner"
 import Redirect from "./Redirect"
 
 export default function LaunchPage() {
-  return <Redirect />
+  return (
+    <div className="custom-fade-in flex min-h-[400px] items-center justify-center">
+      <Redirect />
+      <Spinner className="my-0" />
+    </div>
+  )
 }
