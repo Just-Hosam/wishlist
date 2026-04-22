8b3a3a838369dbfeddb35b0f95d215155b68961b
Just-Hosam <hosam_firas@hotmail.com>
2026-04-22 17:03:12 -0600

    Minor UI updates

--- components/game/Game.tsx
+++ components/game/Game.tsx
@@ -56,7 +56,7 @@ export function Game({
     <div className="custom-slide-up-fade-in">
       {/* HEADER */}
       <header className="mx-auto flex flex-col items-center text-center">
-        <div className="-mt-1 mb-5 h-[280px] w-[210px] overflow-hidden rounded-3xl bg-gray-200 shadow-md">
+        <div className="-mt-1 mb-4 h-[280px] w-[210px] overflow-hidden rounded-3xl bg-gray-200 shadow-md">
           <Image
             src={buildIGDBImageUrl(imageId || "")}
             alt={name || "Game cover"}
@@ -85,7 +85,7 @@ export function Game({
       </header>
 
       {/* PRICES & SUMMARY */}
-      <div className="mt-8 rounded-3xl bg-card p-6 shadow-sm">
+      <div className="mt-8 rounded-2xl bg-card p-6 shadow-sm">
         <div className="flex items-center">
           <Image
             src="/logos/steam.svg"
