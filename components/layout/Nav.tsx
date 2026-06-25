88de23c660bf59492f2c910efd88851b8849d1bc
Just-Hosam <hosam_firas@hotmail.com>
2026-06-25 10:30:32 -0600

    Split chrome and nav

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,17 +1,19 @@
-import { cn } from "@/lib/utils"
-
 interface NavProps {
   children?: React.ReactNode
 }
 
 export function Nav({ children }: NavProps) {
   return (
-    <nav
-      className={cn(
-        "absolute left-0 right-0 top-0 z-30 m-auto flex min-h-[96px] max-w-[1200px] items-center gap-4 bg-gradient-to-b from-background via-background/65 via-55% to-transparent px-5 pb-7 pt-4"
-      )}
-    >
-      {!!children && children}
-    </nav>
+    <>
+      {/* CHROME */}
+      <div
+        className="absolute left-0 right-0 top-0 z-30 m-auto min-h-[96px] max-w-[1200px] bg-gradient-to-b from-background via-background/65 via-55% to-transparent"
+        aria-hidden
+      />
+      {/* NAV BAR */}
+      <nav className="absolute left-0 right-0 top-0 z-40 m-auto flex min-h-[96px] max-w-[1200px] items-center gap-4 px-5 pb-7 pt-4">
+        {!!children && children}
+      </nav>
+    </>
   )
 }
